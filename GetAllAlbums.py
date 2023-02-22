d2aad8728f03e2a37980fbbf878c9fde60b3f095
Jason Harris <jason.harris@epl.ca>
2023-02-22 13:23:02 -0700

    Added venv

--- GetAllAlbums.py
+++ GetAllAlbums.py
@@ -1,21 +1,21 @@
 import csv
 import requests
-import GetToken
+import APIrequest
+#import GetToken
 
 #Get Token needs to run first to get this varible. I'm sure it could all be called here more elegantly
+access_token = APIrequest.access_token
+#access_token = GetToken.access_token
 
-access_token = GetToken.access_token
+#I have a local file called APIrequest with my real credential - saving this so I don't forget I did that. Future me will figure out a better way to work with API credentials and tokens (har har)
 
-# I have a local file called APIrequest with my real credential - saving this so I don't forget I did that. Future me will figure out a better way to work with API credentials and tokens (har har)
-# access_token = APIrequest.access_token
-
-with open('input.csv', 'r') as f:
+with open('input.csv', encoding='utf-8', mode='r') as f:
     reader = csv.reader(f)
     data = list(reader)
 
 for row in data:
-    song_name = row[0]
-    artist_name = row[1]
+    artist_name = row[0]
+    song_name = row[1]
 
     search_url = f'https://api.spotify.com/v1/search?q={song_name}+artist:{artist_name}&type=track'
 
@@ -34,6 +34,6 @@ for row in data:
 
     row.append(album_name)
 
-with open('output.csv', 'w', newline='') as f:
+with open('output.csv', encoding='utf-8', mode='w', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(data)
